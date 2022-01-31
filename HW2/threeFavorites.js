05bfc241b78dd1721bc475b6a7ccd25ee02e072c
michaelbycycle <michaeljwade2@gmail.com>
2022-01-30 19:22:38 -0700

    submission from DW
    Dreamweaver transfer

---
+++ HW2/threeFavorites.js
@@ -0,0 +1,3 @@
+console.log('Three of my favorite websites are ESPN, TouTube, and YouTube Music.')
+console.log('Three of my favorite games are basketball, poker, and Cranium.')
+console.log('Three of my favorite artists are Berry Gordy for creating Motwon Records, Edvard Munch for creating The Scream composition, and John Hughes for his catalog of movies he made in the 1980s and 90s.')
