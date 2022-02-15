18b7a846792e23bc982f8748a3af37f8aaac0e11
michaelbycycle <michaeljwade2@gmail.com>
2022-02-14 19:19:40 -0700

    hw4 sub
    assignment submission

---
+++ HW4/progressions.js
@@ -0,0 +1,36 @@
+/*function myFunction() {
+    let number = prompt("Please enter a number", 1);
+    if (number != null) {
+        switch (number) {
+            case 0:
+              day = "Sunday";
+              break;
+            case 1:
+              day = "Monday";
+              break;
+            case 2:
+               day = "Tuesday";
+              break;
+            case 3:
+              day = "Wednesday";
+              break;
+        }
+    }
+    document.getElementById("demo").innerHTML =
+      "Hello " + number;
+}
+
+
+
+function showQuestion(){
+    var x = document.getElementById("questionDiv");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+*/
+
+
